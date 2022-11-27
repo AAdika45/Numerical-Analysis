e2b77d55474c2d39bf4820a4bbd731f45f403754
AAdika <101105666+AAdika45@users.noreply.github.com>
2022-11-28 05:36:49 +0700

    Update Lagrange_Interpolation.py

--- Lagrange Interpolation/Lagrange_Interpolation.py
+++ Lagrange Interpolation/Lagrange_Interpolation.py
@@ -1,9 +1,14 @@
 ## Lagrange Interpolation
 ## Created by AAdika45 (aslamic.adika45@gmail.com)
 
-def LI(k,l,m):   ## k = x_i (interpolation point) ; l = y_i ; m = x ; LI(k,l,m) = p(x)
-    N = 10       ## input total data (total interpolation points, \sum x_i): N = n+1 = len(x_i)
+
+def LI(k,l,m):   
+    """
+    k = x_i (interpolation point) ; l = y_i ; m = x ; LI(k,l,m) = p(x); 
+    input total data (total interpolation points, \sum x_i): N = n+1 = len(x_i)
+    """
     f = list()
+    N=len(k)
     p=0
     for i in range(0,N):
         L =1 
